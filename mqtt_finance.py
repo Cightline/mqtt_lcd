47bd328e4dd5c575dfc3288786d571f995a4a558
Cightline <stealth@desktop.local>
2017-12-27 15:05:16 -0600

    ...

--- mqtt_finance.py
+++ mqtt_finance.py
@@ -14,6 +14,24 @@ class Finance():
         self.publisher = Publisher()
 
 
+
+    def get_price(self, ticker):
+
+        rsp = requests.get('https://finance.google.com/finance?q=%s&output=json' % (ticker))
+
+        if rsp.status_code != 200 and not use_cache:
+            print('incorrect status code for url: %s, code: [%s]' % (rsp.url, rsp.status_code))
+            return False
+
+        try:
+            fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
+
+        except:
+            print('Unable to load ticker')
+            return False
+
+        return fin_data
+
     def send_ticker_data(self):
 
         if self.config['bitcoin'] == True:
@@ -31,8 +49,26 @@ class Finance():
                 o = float(data['open'])
 
                 c = ((l - o)/o) * 100
+               
+                #4,4,4,4,21,14,4 down_arrow
+                self.publisher.publish(msg='%d %4.2f' % (l, c), title='BTC', type_='bitcoin', alert=False)
                 
-                self.publisher.publish(msg='%s %4.2f' % (l, c), title='BTC', type_='bitcoin', alert=False)
+
+        if 'tickers' not in self.config:
+            return
+
+
+        for ticker in self.config['tickers']:
+            p = self.get_price(ticker)
+
+            if not p:
+                continue
+
+            self.publisher.publish(msg='%4.2f (%4.2f)' % (float(p['l']), float(p['cp'])), 
+                                   title=ticker.upper(), 
+                                   type_='ticker_%s' % (ticker), 
+                                   alert=False)
+
 
 
 if __name__ == '__main__':
--- mqtt_publish.py
+++ mqtt_publish.py
@@ -7,7 +7,7 @@ import paho.mqtt.client as mqtt
 class Publisher():
     def __init__(self):
         
-        with open(os.path.expanduser('~/.config/mqtt_lcd/config.json'), 'r') as cfg:
+        with open(os.path.expanduser('~/.config/mqtt/publish.json'), 'r') as cfg:
             self.config = json.load(cfg)
 
         self.client = mqtt.Client()
@@ -26,10 +26,10 @@ class Publisher():
         self.client.subscribe('pi/#')
 
 
-
     def publish(self, msg, title, type_, alert):
-        infot = self.client.publish('pi', json.dumps({'msg':msg, 'title':title, 'type':type_, 'alert':alert}), qos=2)
+        j_data = json.dumps({'msg':msg, 'title':title, 'type':type_, 'alert':alert})
+        infot = self.client.publish('pi', j_data, qos=2)
 
         infot.wait_for_publish()
 
-        print('message published')
+        print('message published: %s' % (j_data))
